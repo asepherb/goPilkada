c7aebfdcc37c2caeda6ff74f076914ea66300317
asepherb <asep.herbasuki@reservasi.com>
2018-08-02 04:02:03 +0700

    add file pilkada

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	e := echo.New()
 
-	e.GET("/tasks", func(c echo.Context) error { return c.JSON(200, "REST API") })
+	e.GET("/tasks", func(c echo.Context) error { return c.JSON(200, "REST API PILKADA") })
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
